409ac3e9f2026a2a3722a51185ce330ec6b25213
Dmitrii Tokarev <truvor.skameikin@gmail.com>
2017-03-01 11:45:02 +0300

    Shorten app id

--- discovery_example.cpp
+++ discovery_example.cpp
@@ -11,7 +11,7 @@
 #endif
 
 const int kPort = 12021;
-const uint64_t kApplicationId = 57681412;
+const uint64_t kApplicationId = 7681412;
 
 void Usage(int argc, char* argv[]) {
   std::cout << "Usage: " << argv[0] << " {discover|discoverable|both} [user_data]" << std::endl;
